344e90a64c68389e2762e22717e5448b56e5fb12
Mohammed <engmo159@git.com>
2024-06-30 12:09:07 +0300

    gge

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,7 @@ import Feedback from './sections/Feedback'
 
 export default function Home() {
   return (
-    <div
-      className='scrollspy-example'
-      data-bs-spy='scroll'
-      data-bs-target='#navbar'
-      data-bs-root-margin='0px 0px -40%'
-      data-bs-smooth-scroll='true'
-      // tabIndex='0'
-    >
+    <div>
       <Carousel />
       <Products />
       <VideoSection />
